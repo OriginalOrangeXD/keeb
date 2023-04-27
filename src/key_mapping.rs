93dc650b2f7b1dac50fe607da7eb5a6660cd9f22
OriginalOrangeXD <deangelis.robert13@gmail.com>
2023-04-26 23:22:12 -0400

    left

--- src/key_mapping.rs
+++ src/key_mapping.rs
@@ -2,22 +2,22 @@ use crate::{key_codes::KeyCode, NUM_COLS, NUM_ROWS};
 
 #[rustfmt::skip]
 pub const NORMAL_LAYER_MAPPING: [[KeyCode; NUM_ROWS]; NUM_COLS] = [
-    [KeyCode::Empty, KeyCode::Empty, KeyCode::Empty, KeyCode::Empty,     KeyCode::Empty,      KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
-    [KeyCode::Empty, KeyCode::L,     KeyCode::S,     KeyCode::Z,         KeyCode::Backspace,  KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
-    [KeyCode::R,     KeyCode::N,     KeyCode::V,     KeyCode::Right,     KeyCode::RightShift, KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
-    [KeyCode::C,     KeyCode::T,     KeyCode::W,     KeyCode::Up,        KeyCode::Down,       KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
-    [KeyCode::G,     KeyCode::H,     KeyCode::M,     KeyCode::Left,      KeyCode::Empty,      KeyCode::Tab,   KeyCode::RightCmd, KeyCode::Enter],
-    [KeyCode::F,     KeyCode::D,     KeyCode::B,     KeyCode::Backspace, KeyCode::Empty,      KeyCode::Tab,   KeyCode::Empty, KeyCode::Empty],
+    [KeyCode::Empty, KeyCode::Tilde, KeyCode::Empty, KeyCode::Empty,     KeyCode::Empty,     KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
+    [KeyCode::Empty, KeyCode::SingleQuote,     KeyCode::A,  KeyCode::Semicolon, KeyCode::Empty,     KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
+    [KeyCode::Comma, KeyCode::O,     KeyCode::Q,            KeyCode::Left,      KeyCode::Empty,     KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
+    [KeyCode::Period,KeyCode::E,     KeyCode::J,            KeyCode::Up,        KeyCode::Down,      KeyCode::Empty, KeyCode::Empty, KeyCode::Empty],
+    [KeyCode::P,     KeyCode::U,     KeyCode::K,       KeyCode::Right,          KeyCode::Fn,KeyCode::RightCmd, KeyCode::Fn, KeyCode::Space],
+    [KeyCode::Y,     KeyCode::I,     KeyCode::X,        KeyCode::Backspace, KeyCode::RightShift,KeyCode::RightCtrl, KeyCode::Empty, KeyCode::Empty],
 ];
 
 
 #[rustfmt::skip]
 pub const FN_LAYER_MAPPING: [[KeyCode; NUM_ROWS]; NUM_COLS] = [
     [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
-    [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
-    [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
-    [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
-    [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
+    [KeyCode::Num1, KeyCode::Num4, KeyCode::Num1, KeyCode::Num1, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
+    [KeyCode::Num7, KeyCode::Num4, KeyCode::Num1, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
+    [KeyCode::Num8, KeyCode::Num5, KeyCode::Num2, KeyCode::Num0, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Fn, KeyCode::Backspace],
+    [KeyCode::Num9, KeyCode::Num6, KeyCode::Num3, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
     [KeyCode::F, KeyCode::G, KeyCode::C, KeyCode::R, KeyCode::Backspace, KeyCode::RightCmd, KeyCode::Backspace, KeyCode::Backspace],
 ];
 
